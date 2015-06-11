f0c358e850277531679a68f6b654d3198a5d8998
Bradley Berger <hippy.berger@gmail.com>
2015-06-10 18:02:29 -0600

    Fixed string_practice.rb file
    [#96698912]

--- methods_and_flow_control/string_practice.rb
+++ methods_and_flow_control/string_practice.rb
@@ -6,7 +6,7 @@ string = 'this is a string to practice with'
 puts string
 puts string.capitalize
 puts string.upcase
-puts "This is a '#{string[10..15]}' to practice with"
+puts string.capitalize.gsub('string', "'string'")
 puts "The string '#{string}' has #{string.length} characters"
 puts string.reverse
 puts string[20...29] * 2 + string[20..27]+'!'
