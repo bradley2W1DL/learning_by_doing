1aa7f7062344ccf6e5f003c1c5ea90dec1352161
Bradley Berger <hippy.berger@gmail.com>
2015-06-12 08:57:52 -0600

    Fixed some guard errors in array_practice.rb
    [#96733706]

--- arrays/array_practice.rb
+++ arrays/array_practice.rb
@@ -16,6 +16,6 @@ array.delete(7)
 puts "If we delete 5, 6 and 7 from the array, we're left with [#{array.join(',')}]"
 puts "If we add 5 at the beginning of the array, we're left with [#{array.insert(0, 5).join(',')}]"
 puts "If we add 6 at the end of the array, we're left with [#{array.push(6).join(',')}]"
-puts "Only the elements #{array.delete_if {|element| element < 9}} are > 8."
+puts "Only the elements #{array.delete_if { |element| element < 9 } } are > 8."
 array.clear
 puts "If we remove all the elements, then the length of the array is #{array.length}"
