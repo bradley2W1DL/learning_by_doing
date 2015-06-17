b958523b29ecad6e2ab7bd09ea36853ab67b9b99
Bradley Berger <hippy.berger@gmail.com>
2015-06-17 11:05:58 -0600

    Modified cash_register.rb Used `sprintf` instead of `.round`
    [#96738396]

--- objects/cash_register.rb
+++ objects/cash_register.rb
@@ -9,7 +9,7 @@ class CashRegister
   end
 
   def total
-    @total.round(2)
+    sprintf("%.2f", @total)
   end
 
   def purchase(amount)
@@ -20,10 +20,10 @@ class CashRegister
   def pay(amount)
     if amount < @total
       @total -= amount
-      puts "Your new total is: $#{@total.round(2)}"
+      puts "Your new total is: $#{sprintf("%.2f", @total)}"
     else
       @total -= amount
-      puts "Your change is: $#{@total.abs.round(2)}"
+      puts "Your change is: $#{sprintf("%.2f", @total.abs)}"
       @total = 0.00 # resets total to zero
     end
   end
@@ -33,8 +33,9 @@ register = CashRegister.new
 puts register.total
 register.purchase(3.78)
 puts register.total
-register.purchase(8.45)
+register.purchase(5.22)
 puts register.total
 register.pay(5.00)
 puts register.total
-register.pay(10)
+register.pay(9.45)
+puts register.total
